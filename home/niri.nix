199bb10b4185f77c583daaa501920a0f26c8a806
shibahaczix <shibahaczix@gmail.com>
2024-08-23 21:03:58 +0200

    Rebase to self hosted server

--- home/niri.nix
+++ home/niri.nix
@@ -3,24 +3,20 @@
 {
   programs.niri = {
     enable = true;
-    settings = {
-      #outputs."HDMI-A-1".scale = 2.0;
-      #window-rule = {
-      #  geometry-corner-radius = 12;
-      #  clip-to-geometry = true;
-      #};
-      #binds = {
-      #  "Mod+D".action.spawn = "fuzzel";
-      #  "Mod+1".action.focus-workspace = 1;
-      #};
-    };
+    #settings = {
+    #  outputs."HDMI-A-1".scale = 2.0;
+    #  window-rule = {
+    #    geometry-corner-radius = 12;
+    #    clip-to-geometry = true;
+    #  };
+    #  binds = {
+    #    "Mod+D".action.spawn = "fuzzel";
+    #    "Mod+1".action.focus-workspace = 1;
+    #  };
+    #};
     config = ''
       output "HDMI-A-1" {
-        scale 2.0
-      }
-      window-rule {
-        geometry-corner-radius 12
-        clip-to-geometry true
+        scale 1.0
       }
       binds {
         Mod+Space { spawn "fuzzel"; }
