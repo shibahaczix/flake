4ea94a776550f27194696e9679de52706bdeed6d
shibahaczix <shibahaczix@gmail.com>
2025-03-12 18:21:59 +0100

    revert

--- home/git.nix
+++ home/git.nix
@@ -6,9 +6,11 @@
     enable = true;
     userName = "shibahaczix";
     userEmail = "shibahaczix@gmail.com";
-    signing.format = "ssh";
-    signing.signByDefault = true;
-    extraConfig.user.signingkey = "~/.ssh/id_ed25519";
+    signing = {
+      format = "ssh";
+      signByDefault = true;
+      key = "~/.ssh/id_ed25519";
+    };
     extraConfig.core.editor = "hx";
   };
 }
